d4b473e5d0166eceb9f6ace420f9d82186578a90
Tom Barber <tom@spicule.co.uk>
2022-06-28 21:01:10 +0000

    quick test

--- build/eksctl/deploy.sh
+++ build/eksctl/deploy.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 export AWS_ACCESS_KEY_ID="${{ github.event.inputs.AWS_ACCESS_KEY_ID }}" && export AWS_SECRET_ACCESS_KEY="${{ github.event.inputs.AWS_ACCESS_KEY_SECRET }}" && export AWS_SESSION_TOKEN="${{ github.event.inputs.AWS_SESSION_TOKEN }}"
