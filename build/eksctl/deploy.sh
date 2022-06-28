259720ad1611fdf5c38b90c400fec7591c46419a
Tom Barber <tom@spicule.co.uk>
2022-06-28 21:05:22 +0000

    quick test

--- build/eksctl/deploy.sh
+++ build/eksctl/deploy.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY_ID && export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_ACCESS_KEY_SECRET && export AWS_SESSION_TOKEN=$INPUT_AWS_SESSION_TOKEN
+eksctl create cluster -f build/eksctl/eksctl-config.yaml
