367dcbee2422fe06d49f595a45b3924e43951f59
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-08-18 12:01:44 -0700

    added configs to store tf state on s3

--- terraform-unity/main.mcp.tf
+++ terraform-unity/main.mcp.tf
@@ -26,6 +26,12 @@ terraform {
   }
 
   required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "unity-cs-tf-state-dev"
+    key    = "tf/state/dev/sps"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {
