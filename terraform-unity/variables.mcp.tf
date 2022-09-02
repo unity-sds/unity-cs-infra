9a5decf561e32287626185f4f7ffe0bf0ba80628
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-09-02 15:10:03 -0700

    updating deployment workflow to use parameterized environments

--- terraform-unity/variables.mcp.tf
+++ terraform-unity/variables.mcp.tf
@@ -4,10 +4,10 @@
 
 variable "ami_name" { default = "unity-ubuntu" }
 #variable "ami_id" { default = "ami-0688ba7eeeeefe3cd" }
-variable "ami_id" { default = "ami-0966013e814042b23" }
+#variable "ami_id" { default = "ami-0966013e814042b23" }
 #variable "ami_id" { default = "ami-04505e74c0741db8d" }
 variable "ami_key_pair_name" { default = "unity-cs-mcp-smolensk" }
-variable "vpc_id" { default = "vpc-0106218dbddd3a753" }
+#variable "vpc_id" { default = "vpc-0106218dbddd3a753" }
 
 data "aws_vpc" "unity-test-env" {
   id         = var.vpc_id
@@ -16,13 +16,16 @@ data "aws_vpc" "unity-test-env" {
 
 // Use existing subnets from MCP
 data "aws_subnet" "subnet-uno" {
-  id = "subnet-059bc4f467275b59d"
+#  id = "subnet-059bc4f467275b59d"
+  id = var.subnet_one_id
 }
 
 data "aws_subnet" "subnet-two" {
-  id = "subnet-0ebdd997cc3ebe58d"
+#  id = "subnet-0ebdd997cc3ebe58d"
+  id = var.subnet_two_id
 }
 
 data "aws_internet_gateway" "infra-env-gw" {
-  internet_gateway_id = "igw-0622379cb99c03649"
+#  internet_gateway_id = "igw-0622379cb99c03649"
+  internet_gateway_id = var.igw_id
 }
