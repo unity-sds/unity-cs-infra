1ba1636c5f05f50ee52745ece3f3e85cb3a2fcdf
Tom Barber <tom@spicule.co.uk>
2022-09-12 10:03:18 +0000

    test eks

--- ekstest/main.tf
+++ ekstest/main.tf
@@ -306,20 +306,20 @@ module "eks" {
 ################################################################################
 
 
-module "disabled_fargate_profile" {
-  source = "../../modules/fargate-profile"
-
-  create = false
-}
-
-module "disabled_eks_managed_node_group" {
-  source = "../../modules/eks-managed-node-group"
-
-  create = false
-}
-
-module "disabled_self_managed_node_group" {
-  source = "../../modules/self-managed-node-group"
-
-  create = false
-}
+#module "disabled_fargate_profile" {
+#  source = "../../modules/fargate-profile"
+#
+#  create = false
+#}
+#
+#module "disabled_eks_managed_node_group" {
+#  source = "../../modules/eks-managed-node-group"
+#
+#  create = false
+#}
+#
+#module "disabled_self_managed_node_group" {
+#  source = "../../modules/self-managed-node-group"
+#
+#  create = false
+#}
