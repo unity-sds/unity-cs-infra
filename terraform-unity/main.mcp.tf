d6b2353aff9317076fb55f4012af0de8add9a020
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-09-15 13:42:34 -0700

    hackathon

--- terraform-unity/main.mcp.tf
+++ terraform-unity/main.mcp.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      #version = "~> 3.27"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "3.1.0"
+    # }
 
-    local = {
-      source  = "hashicorp/local"
-      version = "2.1.0"
-    }
+    # local = {
+    #   source  = "hashicorp/local"
+    #   version = "2.1.0"
+    # }
 
-    null = {
-      source  = "hashicorp/null"
-      version = "3.1.0"
-    }
+    # null = {
+    #   source  = "hashicorp/null"
+    #   version = "3.1.0"
+    # }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
