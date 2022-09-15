f292f7d0a62c1cb3b43aec03304d4f54e1673c18
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-09-15 11:12:52 -0700

    adding test code for spinning up eks clusters in terraform

---
+++ terraform-unity-eks_module/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "my-cluster"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "12.2.0"
+
+  cluster_name    = "${local.cluster_name}"
+  cluster_version = "1.17"
+  subnet_id = aws_subnet.subnet-uno.id
+
+  vpc_id = data.aws_vpc.id
+
+  node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "m5.large"
+    }
+  }
+
+  write_kubeconfig   = true
+  config_output_path = "./"
+}
