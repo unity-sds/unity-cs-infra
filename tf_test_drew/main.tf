acff6da4574cffdd845a693073ced25055021345
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-12-20 15:00:47 -0800

    fixing terraform bugs

--- tf_test_drew/main.tf
+++ tf_test_drew/main.tf
@@ -27,11 +27,11 @@ terraform {
 
   required_version = ">= 0.14.9"
 
-  backend "s3" {
-    bucket = "unity-cs-tf-state-dev"
-    key    = "tf_state"
-    region = "us-west-2"
-  }
+#   backend "s3" {
+#     bucket = "unity-cs-tf-state-dev"
+#     key    = "tf_state"
+#     region = "us-west-2"
+#   }
 }
 
 provider "aws" {
