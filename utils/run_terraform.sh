47dddfad00940c84cd1b8b4ee902c70ddab04fd7
Barber <barber@MT-306070.jpl.nasa.gov>
2023-04-20 15:47:19 +0100

    update vars

--- utils/run_terraform.sh
+++ utils/run_terraform.sh
@@ -5,6 +5,8 @@ cd $1
 # Init
 terraform init
 
+# Setup workspace
+terraform workspace select ${TARGET_ENV}_${TARGET_STAGE}_${TARGET_PROJECT}_${TARGET_OWNER} || terraform workspace new ${TARGET_ENV}_${TARGET_STAGE}_${TARGET_PROJECT}_${TARGET_OWNER}
 # Plan
 terraform plan -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars -out=./tf.plan
 
