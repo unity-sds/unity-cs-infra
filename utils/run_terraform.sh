7c833d2d138fca9b79b6ad0958716d8583c68f2e
Barber <barber@MT-306070.jpl.nasa.gov>
2023-04-20 15:55:01 +0100

    update vars

--- utils/run_terraform.sh
+++ utils/run_terraform.sh
@@ -11,4 +11,4 @@ terraform workspace select ${TARGET_ENV}_${TARGET_STAGE}_${TARGET_PROJECT}_${TAR
 terraform plan -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars -out=./tf.plan
 
 # Apply
-terraform apply -auto-approve -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars ./tf.plan
+terraform apply -auto-approve ./tf.plan
