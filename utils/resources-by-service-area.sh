538c397cfb16425c94032b4885f67bd3f0b59278
Galen Hollins <47542238+galenatjpl@users.noreply.github.com>
2023-04-26 16:13:49 -0700

    added jupyter as an U-ADS case

--- utils/resources-by-service-area.sh
+++ utils/resources-by-service-area.sh
@@ -23,19 +23,24 @@ getNumResourcesNotTaggedWithServiceArea () {
 # 'ServiceArea' tag.
 # =================================================
 getResourcesTaggedWithServiceArea () {
-  #export AWS_PAGER=""
+
+  # Get AWS resources with the specified ServiceArea tag
   RESOURCES=`aws resourcegroupstaggingapi get-resources \
     --tag-filters "Key=ServiceArea,Values=${1}" \
     --query 'ResourceTagMappingList[*].[ResourceARN]' \
     --output text`
+
+  # Count the number of resources found
   RESOURCE_COUNT=`echo "$RESOURCES" | grep -c 'arn:'`
+
+  # Print the results
   echo
   echo "------------ ServiceArea = ${1} -------------- ($RESOURCE_COUNT found):"
   if [[ $RESOURCE_COUNT -gt 0 ]]; then
-    #echo "------------ ServiceArea = ${1} -------------- ($RESOURCE_COUNT found):"
-    #echo "$1 resources ($RESOURCE_COUNT found):"
+    # If resources are found, print their ARNs
     echo "$RESOURCES" | grep "arn:"
   else
+    # If no resources are found, print a message indicating this
     echo "$1 resources (0 found)"
   fi
 }
@@ -73,14 +78,14 @@ getResourcesTaggedWithServiceArea "sps"
 getResourcesTaggedWithServiceArea "ds"
 getNoServiceAreaResources "ucs|galen|ryan|tom|hollins|ramesh|rmaddego|molen|apigw|apigateway" "U-CS"
 getNoServiceAreaResources "uds|cumulus" "U-DS"
-getNoServiceAreaResources "uads|jmcduffi|dockstore|esarkiss|nlahaye" "U-ADS"
+getNoServiceAreaResources "uads|jmcduffi|dockstore|esarkiss|nlahaye|jupyter" "U-ADS"
 getNoServiceAreaResources "usps|u-sps|sps-api|luca|ryan|hysds" "U-SPS"
 getNoServiceAreaResources "bcdp" "U-AS"
 getNoServiceAreaResources "gmanipon|on-demand" "U-OD"
 getNoServiceAreaResources "anil|tapella|natha" "U-UI"
 
 
-export OTHER_RESOURCES=`getNoServiceAreaResources "arn" "OTHER" | grep -Ev "ucs|galen|tom|hollins|ramesh|rmaddego|molen|apigw|apigateway|uds|cumulus|uads|jmcduffi|dockstore|esarkiss|nlahaye|usps|u-sps|sps-api|luca|ryan|hysds|bcdp|gmanipon|on-demand|anil|tapella|natha"`
+export OTHER_RESOURCES=`getNoServiceAreaResources "arn" "OTHER" | grep -Ev "ucs|galen|tom|hollins|ramesh|rmaddego|molen|apigw|apigateway|uds|cumulus|uads|jmcduffi|dockstore|esarkiss|nlahaye|jupyter|usps|u-sps|sps-api|luca|ryan|hysds|bcdp|gmanipon|on-demand|anil|tapella|natha"`
 OTHER_RESOURCE_COUNT=`echo "$OTHER_RESOURCES" | grep -c 'arn:'`
 echo
 echo "$OTHER_RESOURCE_COUNT OTHER suspected un-tagged (missing ServiceArea tag) resources:"
