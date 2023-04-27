80ea06302d585c5c107d7ee420c567375677feb9
Barber <barber@MT-306070.jpl.nasa.gov>
2023-04-27 17:08:47 +0100

    update cloud env

---
+++ unity-cloud-env/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "unity-cs-tf-state-dev"
+    key    = "venue_state"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  default_tags {
+    tags = merge(
+      var.default_tags,
+      {
+        runner = "github"
+      },
+    )
+  }
+}
+
+
+variable "default_tags" {
+  default = {
+    Owner   = "Unity CS"
+    Project = "Unity CS"
+  }
+  description = "Default Tags"
+  type        = map(string)
+}
+
+resource "aws_ssm_parameter" "unity-venue" {
+  name  = "/unity/core/venue"
+  type  = "String"
+  value = var.venue
+}
+
+resource "aws_ssm_parameter" "unity-project" {
+  name  = "/unity/core/project"
+  type  = "String"
+  value = var.project
+}
+
+resource "aws_ssm_parameter" "unity-private-subnets" {
+  name = "/unity/network/subnets/private"
+  type = "StringList"
+  value = var.privatesubnets
+}
+
+resource "aws_ssm_parameter" "unity-public-subnets" {
+  name = "/unity/network/subnets/public"
+  type = "StringList"
+  value = var.publicsubnets
+}
+
+locals {
+  ssm_parameters_map = { for param in var.ssm_parameters : param.name => param }
+}
+
+
+resource "aws_ssm_parameter" "example" {
+  for_each = local.ssm_parameters_map
+
+  name  = each.key
+  type  = each.value.type
+  value = each.value.value
+}
---
+++ unity-cloud-env/variables.tf
@@ -0,0 +1,40 @@
+variable "ssm_parameters" {
+  description = "A list of SSM parameters to create"
+  type = list(object({
+    name  = string
+    type  = string
+    value = string
+  }))
+  default = [
+    {
+      name  = "parameter1"
+      type  = "String"
+      value = "value1"
+    },
+    {
+      name  = "parameter2"
+      type  = "String"
+      value = "value2"
+    },
+  ]
+}
+
+variable "venue" {
+  description = "The target venue"
+  type = string
+}
+
+variable "project" {
+  description = "The target project"
+  type = string
+}
+
+variable "privatesubnets" {
+  description = "The preferred private subnets"
+  type = string
+}
+
+variable "publicsubnets" {
+  description = "The preferred public subnets"
+  type = string
+}
