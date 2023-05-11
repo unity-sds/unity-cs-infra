0bbe544506e8b58f2c6d2626ce901a724772fbeb
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-05-10 20:09:01 -0700

    add destroy option to software deploy

--- utils/run_terraform.sh
+++ utils/run_terraform.sh
@@ -7,8 +7,15 @@ terraform init
 
 # Setup workspace
 terraform workspace select ${TARGET_ENV}_${TARGET_STAGE}_${TARGET_PROJECT}_${TARGET_OWNER} || terraform workspace new ${TARGET_ENV}_${TARGET_STAGE}_${TARGET_PROJECT}_${TARGET_OWNER}
-# Plan
-terraform plan -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars -out=./tf.plan
 
-# Apply
-terraform apply -auto-approve ./tf.plan
+if [[ $2 == "destroy" ]]
+then
+    # Destroy
+    terraform destroy -auto-approve 
+else
+    # Plan
+    terraform plan -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars -out=./tf.plan
+
+    # Apply
+    terraform apply -auto-approve ./tf.plan
+fi
