1993144fb7743c26799ebd8cc751eb4b12d14f28
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-05-23 13:12:31 -0700

    only pass needed values to software deploy, use ssm for rest of values

--- utils/run_terraform.sh
+++ utils/run_terraform.sh
@@ -11,10 +11,10 @@ terraform workspace select ${TARGET_ENV}_${TARGET_STAGE}_${TARGET_PROJECT}_${TAR
 if [[ $2 == "destroy" ]]
 then
     # Destroy
-    terraform destroy -auto-approve -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars
+    terraform destroy -auto-approve -var-file=gh_actions.tfvars 
 else
     # Plan
-    terraform plan -var-file=MCP-DEV.tfvars -var-file=gh_actions.tfvars -var-file=mcp.tfvars -out=./tf.plan
+    terraform plan -var-file=gh_actions.tfvars -out=./tf.plan
 
     # Apply
     terraform apply -auto-approve ./tf.plan
