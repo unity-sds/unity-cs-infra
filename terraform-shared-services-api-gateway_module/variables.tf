fd90b30ba73f041f7a5a8765788e92c5fc28c7be
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2023-06-20 10:28:30 -0700

    feat: Shared services API Gateway terraform
    Refer to task unity-cs #195

---
+++ terraform-shared-services-api-gateway_module/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  type        = string
+  description = "Region"
+  default     = "us-west-2"
+}
+
+variable "rest_api_name" {
+  type        = string
+  description = "REST API Name"
+  default     = "Unity Shared Services REST API Gateway"
+}
+
+variable "rest_api_description" {
+  type        = string
+  description = "REST API Description"
+  default     = "Unity Shared Services REST API Gateway"
+}
+
+variable "venue" {
+  type        = string
+  description = "Venue for deployment"
+  default     = "dev"
+}
+
+variable "rest_api_stage" {
+  type        = string
+  description = "REST API Stage Name"
+  default     = "dev"
+}
