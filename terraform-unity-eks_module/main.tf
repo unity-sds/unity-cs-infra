a6dc367f812e91bcecaab46020617aac6d0c77b7
Tom Barber <tom@spicule.co.uk>
2023-08-15 16:08:23 +0100

    Update main.tf

--- terraform-unity-eks_module/main.tf
+++ terraform-unity-eks_module/main.tf
@@ -14,13 +14,24 @@ variable "subnets" {
   type = map(list(string))
 }
 
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+variable "cluster_sg" {
+  type = string
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+variable "node_sg" {
+  type = string
+}
+
+variable "eks_iam_role" {
+  type = string
+}
+
+variable "eks_iam_node_role" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
 }
 
 #provider "kubernetes" {
@@ -42,27 +53,59 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
   subnet_ids       = var.subnets.private
 
   vpc_id = var.vpc_id
+
+  cluster_security_group_id = var.cluster_sg
+  create_cluster_security_group = false
+  create_node_security_group = false
+  create_iam_role = false
+  enable_irsa = false
+  iam_role_arn = var.eks_iam_role
+  node_security_group_id = var.node_sg
 
   eks_managed_node_group_defaults = {
     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
 
   eks_managed_node_groups = {
-    blue = {}
+#    blue = {
+#      create_iam_role = false
+#      iam_role_arn = var.eks_iam_node_role
+#      ami_id = "ami-0c0e3c5bfa15ba56b"
+#      use_custom_launch_template = true
+#
+#    }
     green = {
+      create_iam_role = false
+      iam_role_arn = var.eks_iam_node_role
+
       min_size     = 1
       max_size     = 10
       desired_size = 1
 
-      ami_type = "AL2_x86_64"
-      ami_id = "ami-00c008cd43492a617"
+      ami_id = "ami-0c0e3c5bfa15ba56b"
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
+      enable_bootstrap_user_data = true
+
     }
   }
+  tags = var.tags
 }
 
 #module "irsa-ebs-csi" {
