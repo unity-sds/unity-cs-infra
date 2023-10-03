f9420c0bd7b4e791c96645db6d8b4e349b1b2336
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2023-10-03 14:34:31 -0700

    updating nightly tests to use ondemand cloudformation script

---
+++ nightly_tests/nightly_tests_ondemand/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -f nightly_output.txt
+
+bash deploy.sh
+#bash step2.sh &
+#sleep 10
+bash destroy.sh
+
+cat nightly_output.txt
+
+OUTPUT=$(cat nightly_output.txt)
+
+WEBHOOK_URL="https://hooks.slack.com/workflows/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"
+
+curl -X POST -H 'Content-type: application/json' --data '{"text": "'"${OUTPUT}"'"}' $WEBHOOK_URL
