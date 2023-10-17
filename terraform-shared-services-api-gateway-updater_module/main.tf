4346e4b8046e9a1a62394373837b6f97bb1c9cb3
Ramesh Maddegoda <94033485+ramesh-maddegoda@users.noreply.github.com>
2023-10-17 12:20:02 -0700

    Chore: Improved terraform of shared services API Gateway updater
    Refer to task: unity-cs #195

--- terraform-shared-services-api-gateway-updater_module/main.tf
+++ terraform-shared-services-api-gateway-updater_module/main.tf
@@ -1,33 +1,34 @@
 
 # Reference to the REST API to be updated
 data "aws_api_gateway_rest_api" "rest_api" {
+
+  # Name of the REST API to look up. If no REST API is found with this name, an error will be returned. 
+  # If multiple REST APIs are found with this name, an error will be returned. At the moment there is noi data source to 
+  # get REST API by ID.
   name = var.shared_services_rest_api_name
 }
 
 # 
-# Creates the project API Gateway resource.
-# DEPLOYER SHOULD MODIFY THE path_part TO BE THE PROJECT NAME (e.g. "soundersips")
-#
-# TODO: move the path_part to a variable in variables.tf
-#
-resource "aws_api_gateway_resource" "sample_rest_api_resource" {
+# Creates the project API Gateway resource to be pointed to a project level API gateway.
+# DEPLOYER SHOULD MODIFY THE VARIABLE var.resource_for_project TO BE THE PROJECT NAME (e.g. "soundersips")
+resource "aws_api_gateway_resource" "rest_api_resource_for_project" {
   rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
   parent_id   = data.aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "sample_rest_api"
+  path_part   = var.resource_for_project
 }
 
 #
 # Creates the wildcard path (proxy+) resource, under the project resource 
 #
-resource "aws_api_gateway_resource" "sample_rest_api_proxy_resource" {
+resource "aws_api_gateway_resource" "rest_api_resource_for_project_proxy_resource" {
   rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
-  parent_id   = aws_api_gateway_resource.sample_rest_api_resource.id
+  parent_id   = aws_api_gateway_resource.rest_api_resource_for_project.id
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "sample_rest_api_proxy_resource_method" {
+resource "aws_api_gateway_method" "rest_api_resource_for_project_proxy_resource_method" {
   rest_api_id   = data.aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.sample_rest_api_proxy_resource.id
+  resource_id   = aws_api_gateway_resource.rest_api_resource_for_project_proxy_resource.id
   http_method   = "ANY"
   authorization = "NONE"
   request_parameters = {
@@ -35,12 +36,12 @@ resource "aws_api_gateway_method" "sample_rest_api_proxy_resource_method" {
   }
 }
 
-resource "aws_api_gateway_integration" "sample_rest_api_proxy_resource_method_integration" {
+resource "aws_api_gateway_integration" "rest_api_resource_for_project_proxy_resource_method_integration" {
   rest_api_id   = data.aws_api_gateway_rest_api.rest_api.id
-  resource_id          = aws_api_gateway_resource.sample_rest_api_proxy_resource.id
-  http_method          = aws_api_gateway_method.sample_rest_api_proxy_resource_method.http_method
+  resource_id          = aws_api_gateway_resource.rest_api_resource_for_project_proxy_resource.id
+  http_method          = aws_api_gateway_method.rest_api_resource_for_project_proxy_resource_method.http_method
   type                 = "HTTP_PROXY"
-  uri                  = var.sample_rest_api_integration_uri
+  uri                  = var.project_leveL_rest_api_integration_uri
   integration_http_method = "ANY"
 
   cache_key_parameters = ["method.request.path.proxy"]
@@ -52,9 +53,9 @@ resource "aws_api_gateway_integration" "sample_rest_api_proxy_resource_method_in
 
 }
 
-resource "aws_api_gateway_method" "sample_rest_api_get_method" {
+resource "aws_api_gateway_method" "rest_api_get_options_method" {
   rest_api_id   = data.aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.sample_rest_api_resource.id
+  resource_id   = aws_api_gateway_resource.rest_api_resource_for_project.id
   http_method   = "OPTIONS"
   authorization = "NONE"
   request_parameters = {"method.request.header.Authorization" = true}
@@ -62,13 +63,30 @@ resource "aws_api_gateway_method" "sample_rest_api_get_method" {
 
 resource "aws_api_gateway_integration" "sample_rest_api_get_method_integration" {
   rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.sample_rest_api_resource.id
-  http_method = aws_api_gateway_method.sample_rest_api_get_method.http_method
+  resource_id = aws_api_gateway_resource.rest_api_resource_for_project.id
+  http_method = aws_api_gateway_method.rest_api_get_options_method.http_method
 
   type        = "MOCK"
 }
 
+# The Shared Services API Gateway deployment
+resource "aws_api_gateway_deployment" "shared_services_api_gateway_deployment" {
+  rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
+  stage_name        = var.rest_api_stage
+  stage_description = "Deployed at ${timestamp()}"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [ aws_api_gateway_integration.rest_api_resource_for_project_proxy_resource_method_integration ]
+}
+
+
+
+# The following section is commented out, because it is not required for RESTful APIs
+
+/*
 # Sample Website Proxy
 resource "aws_api_gateway_resource" "sample_website" {
   rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
@@ -117,3 +135,5 @@ resource "aws_api_gateway_deployment" "api-gateway-deployment" {
   rest_api_id = data.aws_api_gateway_rest_api.rest_api.id
   stage_name  = var.rest_api_stage
 }
+
+*/
--- terraform-shared-services-api-gateway-updater_module/variables.tf
+++ terraform-shared-services-api-gateway-updater_module/variables.tf
@@ -11,28 +11,42 @@ variable "shared_services_rest_api_name" {
 }
 
 #
-# Overwrite the default value below with the project-level API Gateway, including the stage.
+# Overwrite the default value below with the project-level API Gateway values, including the stage.
 # This is the the API Gateway route that automatically gets created by the 
 # Management Console when it boots up for the first time.
 # This represents the main entry point from the shared services API Gateway,
 # into the project-level API Gateway stage.
 #
-# This will create route under the project resource (this is created in main.tf)
+# This will create a project level resource under the root level of API Gateway(this is created in main.tf)
 #
-variable "sample_rest_api_integration_uri" {
+
+# Specify a name to identify the project name that this resource is pointing to.
+variable "resource_for_project" {
   type = string
-  description = "Sample REST API Integration URI"
-  default = "https://sample-rest-api-id.execute-api.us-west-2.amazonaws.com/dev/{proxy}"
+  description = "An API Gateway resource to identify the Project Name that this specific resource is integrated with"
+  default = "unity-example-project-test"
 }
 
-variable "sample_website_integration_uri" {
+# Specify the URL of the project level API gateway root level with stage name (E.g.: dev) and {proxy} in the URL below.
+# E.g.: If the URL of the project level API Gateway is https://example-rest-api-id.execute-api.us-west-2.amazonaws.com/ then
+# this variable should have the value https://example-rest-api-id.execute-api.us-west-2.amazonaws.com/dev/{proxy}
+variable "project_leveL_rest_api_integration_uri" {
   type = string
-  description = "Sample Website Integration URI"
-  default = "https://www.wikipedia.org"
+  description = "URL of the project level API gateway to be integrated with"
+  default = "https://<API-Gateway-Id>.execute-api.us-west-2.amazonaws.com/dev/{proxy}"
 }
 
+# The deployment stage of the Shared Services API Gateway
 variable "rest_api_stage" {
   type        = string
   description = "REST API Stage Name"
   default     = "dev"
 }
+
+
+# The following variable is commented out, because it is not required for RESTful APIs
+# variable "sample_website_integration_uri" {
+#   type = string
+#   description = "Sample Website Integration URI"
+#   default = "https://www.wikipedia.org"
+# }
