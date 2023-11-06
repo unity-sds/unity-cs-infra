eb69f60392a3bdbd852267e2bb09f42e5844f8d9
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2023-11-06 10:19:51 -0800

    Add docker image

--- nightly_tests/nightly_tests_ondemand/run.sh
+++ nightly_tests/nightly_tests_ondemand/run.sh
@@ -56,6 +56,13 @@ aws cloudformation describe-stack-events --stack-name ${STACK_NAME} >> cloudform
 
 # run selenium test on management console
 export MANAGEMENT_CONSOLE_URL=$(aws cloudformation describe-stacks --stack-name unity-cs-nightly-management-console --query "Stacks[0].Outputs[?OutputKey=='ManagementConsoleURL'].OutputValue" --output text)
+echo "$MC_USERNAME"
+echo "$MC_PASSWORD"
+echo "$MANAGEMENT_CONSOLE_URL"
+
+sudo docker pull selenium/standalone-chrome
+sudo docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome
+sleep 10
 
 python3 selenium_test_management_console.py 
 # sleep 10
