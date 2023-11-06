e8855ba4e313e7c3f8fc92ab7a5f3f8df61e7970
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2023-11-05 22:33:23 -0800

    Remove pytest from script

--- nightly_tests/nightly_tests_ondemand/selenium_test_management_console.py
+++ nightly_tests/nightly_tests_ondemand/selenium_test_management_console.py
@@ -1,36 +1,12 @@
 import os
 import time
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from urllib.parse import urlparse, urlunparse
 
-# Retrieve the credentials and the URL from the environment variables
-mc_username = os.getenv('MC_USERNAME')
-mc_password = os.getenv('MC_PASSWORD')
-management_console_url = os.getenv('MANAGEMENT_CONSOLE_URL')
-
-# Parse the original URL
-parsed_url = urlparse(management_console_url)
-
-# Reconstruct the URL with the username and password included
-new_netloc = f"{mc_username}:{mc_password}@{parsed_url.hostname}"
-if parsed_url.port:
-    new_netloc += f":{parsed_url.port}"
-
-# Construct the new URL
-new_url = urlunparse((parsed_url.scheme, new_netloc, parsed_url.path, parsed_url.params, parsed_url.query, parsed_url.fragment))
-
-
-URL_WITH_CRED = new_url
-URL_WITHOUT_CRED = os.getenv('MANAGEMENT_CONSOLE_URL')
-
-IMAGE_DIR = 'selenium_unity_images'
-
-@pytest.fixture(scope='module')
-def driver():
+# Function to create a new Selenium driver
+def create_driver():
     options = Options()
     # options.add_argument('--headless') # Uncomment this if you run headless mode
     options.add_argument('--no-sandbox')
@@ -44,44 +20,90 @@ def driver():
         command_executor=grid_url,
         options=options
     )
+    return driver
 
-    # Your existing code to create directory for images
-    if not os.path.exists(IMAGE_DIR):
-        os.makedirs(IMAGE_DIR)
+# Function to navigate to the management console URL with credentials
+def navigate_to_url_with_cred(driver, url_with_cred, url_without_cred, image_dir):
+    driver.get(url_with_cred)
+    driver.get(url_without_cred)
+    time.sleep(1)  # Wait for the page to load
 
-    # Navigate to the URLs
-    driver.get(URL_WITH_CRED)
-    driver.get(URL_WITHOUT_CRED)
-    time.sleep(1)
+    # Create directory for images if it doesn't exist
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
 
-    yield driver
-    driver.quit() 
+# Function to test login
+def test_login(driver, image_dir):
+    try:
+        screenshot_path = os.path.join(image_dir, 'screenshot_after_login.png')
+        driver.save_screenshot(screenshot_path)
+        assert driver.current_url.endswith('/ui/landing'), "Navigation to home page failed"
+        assert driver.title == 'Unity Management Console', "The page title should be Unity Management Console"
+        print("Login Test: PASSED")
+    except AssertionError as e:
+        print(f"Login Test: FAILED - {e}")
 
-def test_login(driver):
-    # Assert the page title to check the navigation worked as expected
-    screenshot_path = os.path.join(IMAGE_DIR, 'screenshot_after_login.png')
-    driver.save_screenshot(screenshot_path)
+# Function to test clicking the Go! button
+def test_click_go_button(driver, image_dir):
+    try:
+        go_button = driver.find_element(By.CSS_SELECTOR, 'a.btn.btn-primary[href="/ui/setup"]')
+        go_button.click()
+        time.sleep(1)  # Wait for the page to load
+        
+        screenshot_path = os.path.join(image_dir, 'screenshot_after_clicking_go_button.png')
+        driver.save_screenshot(screenshot_path)
+        assert driver.current_url.endswith('/ui/setup'), "Navigation to setup page failed"
+        print("Click Go Button Test: PASSED")
+    except AssertionError as e:
+        print(f"Click Go Button Test: FAILED - {e}")
 
-    assert driver.current_url.endswith('/ui/landing'), "Navigation to home page failed"
-    assert driver.title == 'Unity Management Console', "The page title should be Unity Management Console"
+# Function to test logout
+def test_logout(driver, image_dir):
+    try:
+        logout_button = driver.find_element(By.CSS_SELECTOR, 'a.nav-link[href="/logout"]')
+        logout_button.click()
+        time.sleep(8)  # Wait for the page to load
 
-def test_click_go_button(driver):
-    # Locate the Go! button and click it
-    go_button = driver.find_element(By.CSS_SELECTOR, 'a.btn.btn-primary[href="/ui/setup"]')
-    go_button.click()
-    time.sleep(1)  
-    
-    # Take a screenshot
-    screenshot_path = os.path.join(IMAGE_DIR, 'screenshot_after_clicking_go_button.png')
-    driver.save_screenshot(screenshot_path)
-    
-    assert driver.current_url.endswith('/ui/setup'), "Navigation to setup page failed"
+        screenshot_path = os.path.join(image_dir, 'screenshot_after_logout.png')
+        driver.save_screenshot(screenshot_path)
+        assert driver.current_url.endswith('/logout'), "Logout failed"
+        print("Logout Test: PASSED")
+    except AssertionError as e:
+        print(f"Logout Test: FAILED - {e}")
 
-def test_logout(driver):
-    go_button = driver.find_element(By.CSS_SELECTOR, 'a.nav-link[href="/logout"]')
-    go_button.click()
-    time.sleep(8)
+# Main execution
+if __name__ == '__main__':
+    IMAGE_DIR = 'selenium_unity_images'
+    mc_username = os.getenv('MC_USERNAME')
+    mc_password = os.getenv('MC_PASSWORD')
+    management_console_url = os.getenv('MANAGEMENT_CONSOLE_URL')
 
-    screenshot_path = os.path.join(IMAGE_DIR, 'screenshot_after_logout.png')
-    driver.save_screenshot(screenshot_path)
-    assert driver.current_url.endswith('/logout'), "Logout failed"
+    # Construct the URL with credentials
+    parsed_url = urlparse(management_console_url)
+    new_netloc = f"{mc_username}:{mc_password}@{parsed_url.hostname}"
+    if parsed_url.port:
+        new_netloc += f":{parsed_url.port}"
+    new_url = urlunparse((parsed_url.scheme, new_netloc, parsed_url.path, parsed_url.params, parsed_url.query, parsed_url.fragment))
+    URL_WITH_CRED = new_url
+    URL_WITHOUT_CRED = management_console_url
+
+    # Create driver
+    driver = create_driver()
+
+    try:
+        print("Navigating to URLs...")
+        navigate_to_url_with_cred(driver, URL_WITH_CRED, URL_WITHOUT_CRED, IMAGE_DIR)
+        
+        print("Testing Login...")
+        test_login(driver, IMAGE_DIR)
+        
+        print("Testing Click Go Button...")
+        test_click_go_button(driver, IMAGE_DIR)
+        
+        print("Testing Logout...")
+        test_logout(driver, IMAGE_DIR)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Quit the driver after the test
+        driver.quit()
