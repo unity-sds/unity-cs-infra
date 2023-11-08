0696aed2c1b85e10affeedc8baffa3e6b842ef5e
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2023-11-07 19:57:28 -0800

    Update test results output

--- nightly_tests/nightly_tests_ondemand/selenium_test_management_console.py
+++ nightly_tests/nightly_tests_ondemand/selenium_test_management_console.py
@@ -32,19 +32,33 @@ def navigate_to_url_with_cred(driver, url_with_cred, url_without_cred, image_dir
     if not os.path.exists(image_dir):
         os.makedirs(image_dir)
 
+# Function to print results in a tabular format
+def print_table(results):
+    # Determine the width of the table based on the longest test name
+    max_name_length = max(len(result['name']) for result in results)
+    name_width = max(max_name_length, len('Test Name'))
+
+    # Print the header
+    print(f"\n{'Test Name'.ljust(name_width)} | {'Status'}")
+    print(f"{'-' * name_width}-+--------")
+
+    # Print each result
+    for result in results:
+        print(f"{result['name'].ljust(name_width)} | {result['status']}")
+
 # Function to test login
-def test_login(driver, image_dir):
+def test_login(driver, image_dir, results):
     try:
         screenshot_path = os.path.join(image_dir, 'screenshot_after_login.png')
         driver.save_screenshot(screenshot_path)
         assert driver.current_url.endswith('/ui/landing'), "Navigation to home page failed"
         assert driver.title == 'Unity Management Console', "The page title should be Unity Management Console"
-        print("Login Test: PASSED")
+        results.append({'name': 'Login', 'status': 'PASSED'})
     except AssertionError as e:
-        print(f"Login Test: FAILED - {e}")
+        results.append({'name': 'Login', 'status': f'FAILED - {e}'})
 
 # Function to test clicking the Go! button
-def test_click_go_button(driver, image_dir):
+def test_click_go_button(driver, image_dir, results):
     try:
         go_button = driver.find_element(By.CSS_SELECTOR, 'a.btn.btn-primary[href="/ui/setup"]')
         go_button.click()
@@ -53,10 +67,10 @@ def test_click_go_button(driver, image_dir):
         screenshot_path = os.path.join(image_dir, 'screenshot_after_clicking_go_button.png')
         driver.save_screenshot(screenshot_path)
         assert driver.current_url.endswith('/ui/setup'), "Navigation to setup page failed"
-        print("Click Go Button Test: PASSED")
+        results.append({'name': 'Click Go Button', 'status': 'PASSED'})
     except AssertionError as e:
-        print(f"Click Go Button Test: FAILED - {e}")
-    
+        results.append({'name': 'Click Go Button', 'status': f'FAILED - {e}'})
+
 # Main execution
 if __name__ == '__main__':
     IMAGE_DIR = 'selenium_unity_images'
@@ -73,6 +87,9 @@ if __name__ == '__main__':
     URL_WITH_CRED = new_url
     URL_WITHOUT_CRED = management_console_url
 
+    # Create a list to store the results
+    test_results = []
+
     # Create driver
     driver = create_driver()
 
@@ -80,11 +97,11 @@ if __name__ == '__main__':
     print("Navigating to URLs...")
     navigate_to_url_with_cred(driver, URL_WITH_CRED, URL_WITHOUT_CRED, IMAGE_DIR)
     
-    print("Testing Login...")
-    test_login(driver, IMAGE_DIR)
+    test_login(driver, IMAGE_DIR, test_results)
+    test_click_go_button(driver, IMAGE_DIR, test_results)
     
-    print("Testing Click Go Button...")
-    test_click_go_button(driver, IMAGE_DIR)
+    # Print the results in a table
+    print_table(test_results)
     
     # Quit the driver after the test
     driver.quit()
