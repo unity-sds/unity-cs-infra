e1f85ae137cd5d176758bebd8554753b21d08506
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2023-11-25 15:55:48 -0800

    Install Docker in run.sh script

--- nightly_tests/nightly_tests_ondemand/run.sh
+++ nightly_tests/nightly_tests_ondemand/run.sh
@@ -54,6 +54,34 @@ aws cloudformation describe-stack-events --stack-name ${STACK_NAME} >> cloudform
 # run selenium test on management console
 export MANAGEMENT_CONSOLE_URL=$(aws cloudformation describe-stacks --stack-name unity-cs-nightly-management-console --query "Stacks[0].Outputs[?OutputKey=='ManagementConsoleURL'].OutputValue" --output text)
 
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker not installed. Installing Docker..."
+
+    # Add Docker's official GPG key
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Add the repository to Apt sources
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install Docker
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
+    sleep 10
+
+    echo "Docker installed successfully."
+else
+    echo "Docker is already installed."
+fi
+
 sudo docker pull selenium/standalone-chrome
 CONTAINER_ID=$(sudo docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome)
 sleep 10
