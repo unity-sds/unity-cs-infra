bcbd3d1235371e10067168da69d123374696b9ca
hargitayjpl <hargitay@jpl.nasa.gov>
2024-02-19 16:10:21 -0800

    Add SSM paramater smoke test

--- nightly_tests/nightly_tests_ondemand/smoke_test.sh
+++ nightly_tests/nightly_tests_ondemand/smoke_test.sh
@@ -32,24 +32,85 @@
 # ApplicationLoadBalancer
 # PublicLoadBalancerListener
 
-# List of SSM Parameter accessed by cloudformation stack
-ssm_parameters=(
-  "/unity/core/project"
-  "/unity/core/venue"
-  "/unity/cs/account/network/vpc_id"
+
+# SSM parameters used as inputs for the CloudFormation stack
+ssm_parameters_input=(
+  "/unity/testing/nightly/vpc-id"
+  "/unity/testing/nightly/publicsubnet1"
+  "/unity/testing/nightly/publicsubnet2"
+  "/unity/testing/nightly/privatesubnet1"
+  "/unity/testing/nightly/privatesubnet2"
+  "/unity/testing/nightly/instancetype"
+  "/unity/testing/nightly/privilegedpolicyname"
+#  "/unity/testing/nightly/githubtoken"
+  "/unity/testing/nightly/venue"
+  "/unity/testing/nightly/accountname"
+)
+
+# Echo the SSM parameters used for the CloudFormation stack deployment
+echo "SSM Parameters inputted into the CloudFormation stack deployment:"
+
+# Loop through the list of input SSM parameters and echo their names and values
+for param_name in "${ssm_parameters_input[@]}"; do
+  param_value=$(aws ssm get-parameter --name "${param_name}" --query "Parameter.Value" --output text)
+  
+  if [ $? -eq 0 ]; then
+    echo "Input Parameter Name: ${param_name}"
+    echo "Parameter Value: ${param_value}"
+  else
+    echo "Failed to retrieve value for ${param_name}"
+  fi
+  echo "--------------------------------"
+done
+
+
+
+
+
+# SSM Parameters accessed by the CloudFormation stack
+ssm_parameters_accessed=(
   "/mcp/amis/ubuntu2004-cset"
 )
 
-# Loop through the list and fetch each parameter
-for param_name in "${ssm_parameters[@]}"; do
+echo ""
+echo "SSM Parameters accessed by CloudFormation stack:"
+# Loop through the list of accessed parameters and get each parameter
+for param_name in "${ssm_parameters_accessed[@]}"; do
+  param_value=$(aws ssm get-parameter --name "${param_name}" --query "Parameter.Value" --output text)
+  
+  if [ $? -eq 0 ]; then
+    echo "Accessed Parameter Name: ${param_name}"
+    echo "Parameter Value: ${param_value}"
+  else
+    echo "Failed to retrieve value for ${param_name}"
+  fi
+  echo "--------------------------------"
+done
+
+
+
+
+# SSM Parameters created by the CloudFormation stack
+ssm_parameters_created=(
+  "/unity/core/project"
+  "/unity/core/venue"
+  "/unity/cs/account/network/vpc_id"
+)
+
+echo ""
+echo "SSM Parameters created by CloudFormation stack:"
+# Loop through the list of created parameters and get each parameter
+for param_name in "${ssm_parameters_created[@]}"; do
+  # Attempt to fetch the value of the created parameter
   param_value=$(aws ssm get-parameter --name "${param_name}" --query "Parameter.Value" --output text)
   
   # Check if the parameter was successfully retrieved
   if [ $? -eq 0 ]; then
-    echo "Parameter Name: ${param_name}"
+    echo "Created Parameter Name: ${param_name}"
     echo "Parameter Value: ${param_value}"
-    echo "--------------------------------"
   else
-    echo "Failed to retrieve value for ${param_name}"
+    echo "Failed to retrieve or parameter may not exist yet for ${param_name}"
   fi
+  echo "--------------------------------"
 done
+
