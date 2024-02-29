af10ff79f1f3394c2a7c6e7346a9923e3170a2ba
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-02-29 10:39:13 -0800

    Test new trust policy

--- terraform-unity-eks_module/variables.tf
+++ terraform-unity-eks_module/variables.tf
@@ -1,10 +1,10 @@
 variable "tags" {
-  type = map(string)
+  type    = map(string)
   default = {}
 }
 
 variable "deployment_name" {
-  type = string
+  type    = string
   default = "unity-eks"
 }
 
@@ -23,24 +23,24 @@ variable "nodegroups" {
     enable_bootstrap_user_data = optional(bool)
   }))
 
-  default = { 
-    defaultGroup ={
+  default = {
+    defaultGroup = {
       instance_types = ["m5.xlarge"]
-      min_size = 1
-      max_size = 1
-      desired_size = 1
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
     }
   }
 }
 
 variable "aws_auth_roles" {
-    description = "AWS auth roles to associate with the cluster"
-    type = list(object({
-        rolearn = string
-        username = string
-        groups = list(string)
-    })) 
-    default = [ ]
+  description = "AWS auth roles to associate with the cluster"
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = []
 }
 
 variable "cluster_version" {
@@ -48,20 +48,20 @@ variable "cluster_version" {
   default = "1.27"
 }
 
-variable "project"{
+variable "project" {
   description = "The unity project its installed into"
-  type = string
-  default = "UnknownProject"
+  type        = string
+  default     = "UnknownProject"
 }
 
 variable "venue" {
   description = "The unity venue its installed into"
-  type = string
-  default = "UnknownVenue"
+  type        = string
+  default     = "UnknownVenue"
 }
 
 variable "installprefix" {
   description = "The management console install prefix"
-  type = string
-  default = "UnknownPrefix"
+  type        = string
+  default     = "UnknownPrefix"
 }
