711c1dc513614b22f409e3e84a1ade9257d90227
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-02-29 17:05:51 -0800

    Add launch template to merged node groups

--- terraform-unity-eks_module/main.tf
+++ terraform-unity-eks_module/main.tf
@@ -59,14 +59,9 @@ locals {
       min_size                   = ng.min_size != null ? ng.min_size : 1
       max_size                   = ng.max_size != null ? ng.max_size : 10
       desired_size               = ng.desired_size != null ? ng.desired_size : 3
-      ami_id                     = ng.ami_id != null ? ng.ami_id : lookup(local.ami_map, var.cluster_version, local.ami_map["default"])
       instance_types             = ng.instance_types != null ? ng.instance_types : ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
       capacity_type              = ng.capacity_type != null ? ng.capacity_type : "ON_DEMAND"
       iam_role_arn               = ng.iam_role_arn != null ? ng.iam_role_arn : aws_iam_role.cluster_iam_role.arn
-      enable_bootstrap_user_data = true
-      pre_bootstrap_user_data    = <<-EOT
-            sudo sed -i 's/^net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf && sudo sysctl -p |true
-        EOT
       create_launch_template     = false
       use_custom_launch_template = true
       launch_template_id         = aws_launch_template.node_group_launch_template.id
