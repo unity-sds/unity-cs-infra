45a597d5b9aa5520415e1d75433a6cc86b0547d7
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-09-11 12:26:50 -0600

    Adding the paranmeters back to data.tf

--- terraform-unity-eks_module/data.tf
+++ terraform-unity-eks_module/data.tf
@@ -28,18 +28,18 @@ data "aws_ssm_parameter" "eks_ami_1_29" {
   name = "/unity/account/eks/amis/aml2-eks-1-29"
 }
 
-# data "aws_ssm_parameter" "eks_ami_1_27" {
-#   name = "/unity/account/eks/amis/aml2-eks-1-27"
-# }
-#
-# data "aws_ssm_parameter" "eks_ami_1_26" {
-#   name = "/unity/account/eks/amis/aml2-eks-1-26"
-# }
-#
-# data "aws_ssm_parameter" "eks_ami_1_25" {
-#   name = "/unity/account/eks/amis/aml2-eks-1-25"
-# }
-#
+data "aws_ssm_parameter" "eks_ami_1_27" {
+  name = "/unity/account/eks/amis/aml2-eks-1-27"
+}
+
+data "aws_ssm_parameter" "eks_ami_1_26" {
+  name = "/unity/account/eks/amis/aml2-eks-1-26"
+}
+
+data "aws_ssm_parameter" "eks_ami_1_25" {
+  name = "/unity/account/eks/amis/aml2-eks-1-25"
+}
+
 #data "aws_ssm_parameter" "eks_ami_1_24" {
 #  name = "/unity/account/eks/amis/aml2-eks-1-24"
 #}
