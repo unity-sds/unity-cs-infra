f407a151050c6f34cce1e50bf2eed78c94b868f7
Bradley Lunsford <109703335+jpl-btlunsfo@users.noreply.github.com>
2024-12-17 09:47:39 -0800

    fixing typo in terraform-unity-eks_module
    typo didn't subselect the ids, and thus the count would incorrectly trigger

--- terraform-unity-eks_module/main.tf
+++ terraform-unity-eks_module/main.tf
@@ -405,7 +405,7 @@ resource "aws_security_group" "mc_ingress_sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "mc_ingress_rule" {
-  count                        = length(data.aws_security_groups.mc_sg) > 0 ? 1 : 0
+  count                        = length(data.aws_security_groups.mc_sg.ids) > 0 ? 1 : 0
   security_group_id            = aws_security_group.mc_ingress_sg.id
   description                  = "SecurityGroup ingress rule for management console"
   ip_protocol                  = -1
