a4a83440f585c1084ea900459b7985771d7354c8
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2025-02-19 14:47:00 -0800

    wip

--- nightly_tests/run.sh
+++ nightly_tests/run.sh
@@ -183,7 +183,7 @@ echo "  - Monitoring Lambda Version:      $MONITORING_LAMBDA_VERSION"
 echo "---------------------------------------------------------"
 
 export STACK_NAME="unity-management-console-${PROJECT_NAME}-${VENUE_NAME}"
-export GH_BRANCH="main"
+export GH_BRANCH="520-dev"
 TODAYS_DATE=$(date '+%F_%H-%M')
 LOG_DIR=nightly_logs/log_${TODAYS_DATE}
 
