3dc77147a0deaddc5b3dcbabf3774c786288ec3d
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-10-29 17:07:59 -0700

    added = to kubernetes def

--- terraform-unity-eks_module/provider.tf
+++ terraform-unity-eks_module/provider.tf
@@ -1,5 +1,5 @@
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
