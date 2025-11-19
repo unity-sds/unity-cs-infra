e539d12c6335d3942345c1c94034a8849a0c3c63
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-11-19 09:16:08 -0800

    removed use of boostrap

--- terraform-unity-eks_module/locals.tf
+++ terraform-unity-eks_module/locals.tf
@@ -11,20 +11,28 @@ locals {
   }
   mergednodegroups = { for name, ng in var.nodegroups :
     name => {
-      use_name_prefix            = false
-      create_iam_role            = false
-      min_size                   = ng.min_size != null ? ng.min_size : 1
-      max_size                   = ng.max_size != null ? ng.max_size : 10
-      desired_size               = ng.desired_size != null ? ng.desired_size : 3
-      ami_id                     = ng.ami_id != null ? ng.ami_id : lookup(local.ami_map, var.cluster_version, local.ami_map["default"])
-      ami_type                   = "AL2023_x86_64_STANDARD"
-      instance_types             = ng.instance_types != null ? ng.instance_types : ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-      capacity_type              = ng.capacity_type != null ? ng.capacity_type : "ON_DEMAND"
-      iam_role_arn               = ng.iam_role_arn != null ? ng.iam_role_arn : aws_iam_role.cluster_iam_role.arn
-      enable_bootstrap_user_data = true
-      pre_bootstrap_user_data    = <<-EOT
-            sudo sed -i 's/^net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf && sudo sysctl -p |true
-        EOT
+      use_name_prefix  = false
+      create_iam_role  = false
+      min_size         = ng.min_size != null ? ng.min_size : 1
+      max_size         = ng.max_size != null ? ng.max_size : 10
+      desired_size     = ng.desired_size != null ? ng.desired_size : 3
+      ami_id           = ng.ami_id != null ? ng.ami_id : lookup(local.ami_map, var.cluster_version, local.ami_map["default"])
+      ami_type         = "AL2023_x86_64_STANDARD"
+      instance_types   = ng.instance_types != null ? ng.instance_types : ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+      capacity_type    = ng.capacity_type != null ? ng.capacity_type : "ON_DEMAND"
+      iam_role_arn     = ng.iam_role_arn != null ? ng.iam_role_arn : aws_iam_role.cluster_iam_role.arn
+      cloudinit_pre_nodeadm = [
+        {
+          content_type = "text/x-shellscript"
+          content      = <<-EOT
+            #!/bin/bash
+            set -e
+            # Enable IP forwarding for networking
+            sed -i 's/^net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+            sysctl -p || true
+          EOT
+        }
+      ]
       metadata_options = {
         "http_endpoint" : ng.metadata_options != null ? lookup(ng.metadata_options, "http_endpoint", null) : null
         "http_put_response_hop_limit" : ng.metadata_options != null ? lookup(ng.metadata_options, "http_put_response_hop_limit", null) : null
